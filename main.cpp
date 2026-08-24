516a74fd72d7245d0b1896c8e543fc0fb69e7833
realpatato <jason.t.richardii@gmail.com>
2026-08-23 21:22:48 -0400

    created point class

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 #include <string>
+#include <array>
 #include <raylib.h>
+
+class Point {
+    private:
+        float x;
+        float y;
+    public:
+        Point(float x, float y) : x(x), y(y) {}
+
+        float get_x() {
+            return x;
+        }
+
+        float get_y() {
+            return y;
+        }
+};
+
+class Elipse {
+    private:
+        std::array<int, 2> center;
+};
 
 int main() {
     const int screen_width = 1000;
@@ -9,6 +31,8 @@ int main() {
     InitWindow(screen_width, screen_height, "basic window");
 
     int frame = 0;
+    Point p = Point(0, 0);
+    std::cout << p.get_x() << "," << p.get_y() << std::endl; 
 
     while (!WindowShouldClose()) {
         frame++;
