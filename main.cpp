a1c76c947665759996f26b0c6bd1fe5c62b6f4c2
realpatato <jason.t.richardii@gmail.com>
2026-08-23 20:55:49 -0400

    basic raylib set-up

--- main.cpp
+++ main.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
+#include <string>
+#include <raylib.h>
 
 int main() {
-    for (int i = 0; i < 5; i++) {
-        std::cout << i << std::endl;
+    const int screen_width = 1000;
+    const int screen_height = 500;
+
+    InitWindow(screen_width, screen_height, "basic window");
+
+    int frame = 0;
+
+    while (!WindowShouldClose()) {
+        frame++;
+
+        std::string frame_string = std::to_string(frame);
+        const char* frame_char = frame_string.c_str();
+
+        BeginDrawing();
+
+            ClearBackground(RAYWHITE);
+            DrawText(frame_char, 190, 200, 20, LIGHTGRAY);
+
+        EndDrawing();
     }
+
+    CloseWindow();
     return 0;
 }
